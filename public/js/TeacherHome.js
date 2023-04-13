6345f507adcea19ef64f7e07f5231de5375a39fe
bapatanuparth <parthanup@gmail.com>
2023-04-12 22:52:57 -0400

    manager UI

--- public/js/TeacherHome.js
+++ public/js/TeacherHome.js
@@ -1,78 +1,127 @@
-console.log('document loaded')
+console.log("document loaded");
 
 $(document).ready(() => {
+  $("#creatClass").click((e) => {
+    e.preventDefault();
 
-    $('#creatClass').click((e) => {
-        e.preventDefault()
+    var data = {};
 
-        var data = {};
-
-        $('#topClassCreate').serializeArray().forEach((element) => {
-
-            if (element.value) {
-                data[element.name] = element.value;
-            }
-        });
-        console.log(data);
-        $.ajax({
-            type: 'POST',
-            url: '/chatRoom/create',
-            headers: {
-                "Content-Type": "application/json"
-            },
-            data: JSON.stringify(data),
-            dataType: "json",
-            success: function(data) {
-                alert('Chat room created Succesfully')
-            },
-            error: function(e) {
-                alert('Chat room name already taken')
-                console.log(e);
-            }
-        })
-    })
-
-    $.ajax({
-        type: 'GET',
-        url: '/manager/loadHome',
-        success: function(data) {
-            data.forEach(element => {
-
-                if (element.developerId) {
-                    $('#toAppend').prepend(`
-            <div class="append-info">
-            <div class="info-image">
-            <img src="http://localhost:3000/images/${element?.fileUpload?.filename}"  width="200" height="200" >
-            </div>
-            <div class ="info-data">
-            <div class="data-name">Chat Room name : ${element?.name}</div>
-            <div class="data-description"> <b>Description for the document</b> : <br>${element?.description}
-            
-            </div>
-            <div class="data-name"> <b>Uploaded by</b> :${element?.developerId?.name} </div>
-            </div>
-            </div>
-        <div class="border-bottom"></div>`);
-                } else {
-                    $('#toAppend').prepend(`
-            <div class="append-info">
-            <div class="info-image">
-            <img src="http://localhost:3000/images/${element?.fileUpload?.filename}"  width="200" height="200"  >
-            </div>
-            <div class ="info-data">
-            <div class="data-name">Chat Room name : ${element?.name}</div>
-            <div class="data-description"> <b>Description for the document</b> : <br>${element?.description}
-            </div>
-            <div class="data-name"> <b>Uploaded by</b> :${element?.managerId.name} </div>
-            </div>
-            </div>
-        <div class="border-bottom"></div>`);
-
-                }
-            });
-        },
-        error: function() {
-            alert('Error while loading profile')
+    $("#topClassCreate")
+      .serializeArray()
+      .forEach((element) => {
+        if (element.value) {
+          data[element.name] = element.value;
         }
-    })
-})
+      });
+    console.log(data);
+    $.ajax({
+      type: "POST",
+      url: "/chatRoom/create",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      data: JSON.stringify(data),
+      dataType: "json",
+      success: function (data) {
+        alert("Chat room created Succesfully");
+      },
+      error: function (e) {
+        alert("Chat room name already taken");
+        console.log(e);
+      },
+    });
+  });
+
+  $.ajax({
+    type: "GET",
+    url: "/manager/loadHome",
+    success: function (data) {
+      data.forEach((element) => {
+        if (element.developerId) {
+          $("#toAppend").prepend(`
+            <div class="postWrapper">
+                                <div class="postTop">
+                                    <div class="postTopLeft">
+                                        <div class="postChatUsername">
+                                            <span class="postChatroom">
+                                            ${element?.name}
+                                            </span>
+                                            <span class="postUsername">
+                                                ${element?.developerId?.name}
+                                            </span>
+                                        </div>
+                                    </div>
+                                  
+                                </div>
+                                <div class="postCenter">
+                                    <img class="postImg" src="http://localhost:3000/images/${element?.fileUpload?.filename}" alt="" />
+                                    <span class="postText"> This is the description of a post dasifggfasdkjgfhaskjfha lkajsdhflkjhafg jhasdlkjfhaslkj </span>
+                                </div>
+                                 <div class="postBottom">
+                                    <div class="postBottomLeft">
+                                    <img
+                                        class="likeIcon"
+                                        src="http://localhost:3000/img/like.png"
+                                        alt=""
+                            
+                                    />
+                                    <img
+                                        class="likeIcon"
+                                        src="http://localhost:3000/img/heart.png"
+                                        alt=""
+                    
+                                    />
+                                    <span class="postLikeCounter">2 people like this</span>
+                                    </div>
+                                    <div class="postBottomRight">
+                                        <span class="postCommentText">9 comments</span>
+                                    </div>
+                                </div>`);
+        } else {
+          $("#toAppend").prepend(`
+           <div class="postWrapper">
+                                <div class="postTop">
+                                    <div class="postTopLeft">
+                                        <div class="postChatUsername">
+                                            <span class="postChatroom">
+                                            ${element?.name}
+                                            </span>
+                                            <span class="postUsername">
+                                                ${element?.managerId.name}
+                                            </span>
+                                        </div>
+                                    </div>
+                                  
+                                </div>
+                                <div class="postCenter">
+                                    <img class="postImg" src="http://localhost:3000/images/${element?.fileUpload?.filename}" alt="" />
+                                    <span class="postText"> This is the description of a post dasifggfasdkjgfhaskjfha lkajsdhflkjhafg jhasdlkjfhaslkj </span>
+                                </div>
+                                 <div class="postBottom">
+                                    <div class="postBottomLeft">
+                                    <img
+                                        class="likeIcon"
+                                        src="http://localhost:3000/img/like.png"
+                                        alt=""
+                            
+                                    />
+                                    <img
+                                        class="likeIcon"
+                                        src="http://localhost:3000/img/heart.png"
+                                        alt=""
+                    
+                                    />
+                                    <span class="postLikeCounter">2 people like this</span>
+                                    </div>
+                                    <div class="postBottomRight">
+                                        <span class="postCommentText">9 comments</span>
+                                    </div>
+                                </div>`);
+        }
+      });
+    },
+    error: function () {
+      alert("Error while loading profile");
+    },
+  });
+});
--- public/js/TeacherProfile.js
+++ public/js/TeacherProfile.js
@@ -1,68 +1,91 @@
 $(document).ready((e) => {
-    $.ajax({
-        type: 'GET',
-        url: '/manager/profile',
-        success: function(data) {
+  $.ajax({
+    type: "GET",
+    url: "/manager/profile",
+    success: function (data) {},
+    error: function () {
+      alert("Error while loading profile");
+    },
+  });
 
-        },
-        error: function() {
-            alert('Error while loading profile')
+  $("#editProfile").click((e) => {
+    e.preventDefault();
+
+    var data = {};
+
+    $("#editProfileForm")
+      .serializeArray()
+      .forEach((element) => {
+        if (element.value) {
+          data[element.name] = element.value;
         }
-    })
-
-    $('#editProfile').click((e) => {
-        e.preventDefault()
-
-        var data = {};
-
-        $('#editProfileForm').serializeArray().forEach((element) => {
-
-            if (element.value) {
-                data[element.name] = element.value;
-            }
-        });
-
-        $.ajax({
-            type: 'PATCH',
-            url: '/developer/manager/profile/update',
-            headers: {
-                "Content-Type": "application/json"
-            },
-            data: JSON.stringify(data),
-            dataType: "json",
-            success: function(data) {
-                window.location.href = data.redirect;
-            },
-            error: function() {
-                alert('Error while updating developer')
-
-            }
-        })
-    })
+      });
 
     $.ajax({
-        type: 'GET',
-        url: '/manager/chatRoom/getDocument',
-        success: function(data) {
-            data.forEach(element => {
-                $('#toappend').append(`
-              <div class="append-info">
-              <div class="info-image">
-              <img src="http://localhost:3000/images/${element?.fileUpload?.filename}"  width="200" height="200" >
-              </div>
-              <div class ="info-data">
-              <div class="data-name">Chat Room name : ${element?.name}</div>
-              <div class="data-description"> <b>Description for the document</b> : <br>${element?.description}
-              </div>
-              </div>
-              </div>
-          <div class="border-bottom"></div>`);
+      type: "PATCH",
+      url: "/developer/manager/profile/update",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      data: JSON.stringify(data),
+      dataType: "json",
+      success: function (data) {
+        window.location.href = data.redirect;
+      },
+      error: function () {
+        alert("Error while updating developer");
+      },
+    });
+  });
 
-            });
-        },
-        error: function(e) {
-            alert('somthing went wrong while loading document')
-
-        }
-    })
-})
+  $.ajax({
+    type: "GET",
+    url: "/manager/chatRoom/getDocument",
+    success: function (data) {
+      data.forEach((element) => {
+        $("#toappend").append(`
+             <div class="postWrapper">
+                                <div class="postTop">
+                                    <div class="postTopLeft">
+                                        <div class="postChatUsername">
+                                            <span class="postChatroom">
+                                            ${element?.name}
+                                            </span>
+                                            <span class="postUsername">
+                                                ${element?.managerId.name}
+                                            </span>
+                                        </div>
+                                    </div>
+                                  
+                                </div>
+                                <div class="postCenter">
+                                    <img class="postImg" src="http://localhost:3000/images/${element?.fileUpload?.filename}" alt="" />
+                                    <span class="postText"> This is the description of a post dasifggfasdkjgfhaskjfha lkajsdhflkjhafg jhasdlkjfhaslkj </span>
+                                </div>
+                                 <div class="postBottom">
+                                    <div class="postBottomLeft">
+                                    <img
+                                        class="likeIcon"
+                                        src="http://localhost:3000/img/like.png"
+                                        alt=""
+                            
+                                    />
+                                    <img
+                                        class="likeIcon"
+                                        src="http://localhost:3000/img/heart.png"
+                                        alt=""
+                    
+                                    />
+                                    <span class="postLikeCounter">2 people like this</span>
+                                    </div>
+                                    <div class="postBottomRight">
+                                        <span class="postCommentText">9 comments</span>
+                                    </div>
+                                </div>`);
+      });
+    },
+    error: function (e) {
+      alert("somthing went wrong while loading document");
+    },
+  });
+});
